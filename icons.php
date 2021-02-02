ecbf3d8c21531f04c0d96c0fee57e6264c5fda6a
Mark Limuel Fernando <it.marklimuelfernando@gmail.com>
2021-02-02 19:54:17 +0800

    Group Icons

---
+++ icons.php
@@ -0,0 +1,11 @@
+<?php
+
+echo "Group Name: Icons";
+echo "Group Members:";
+echo "Gutierrez Michael";
+echo "Manguerra Pearl Joy";
+echo "Mallari Vince";
+echo "Manio Abigail";
+echo "Fernando Mark Limuel";
+
+?>
